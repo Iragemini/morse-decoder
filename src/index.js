926b9b9271614a14643d4c5054eef1bd1f307b49
Ira Karavaichyk <iragemini@gmail.com>
2020-10-18 17:30:00 +0300

    feat: finish  Morse code decoder task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,59 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let str = expr.toString();
+    let exprToArrayLength = str.length / 10;
+    let exprToArray = [];
+    let result = "";
+    let letter = "";
+    let k = 0 , j = 0;
+    while (k < str.length){
+        let item = str.substring(k, k+10);
 
+        if(j < exprToArrayLength){
+            exprToArray [j] = item;
+        }
+        j++;
+        k = k+10;
+    }
+    for(let i = 0; i < exprToArrayLength; i++){
+      k = 0;
+      let stack = [];
+      codeLetter = exprToArray[i];
+      if(codeLetter.startsWith('*')){
+        result += " ";
+        continue;
+      }
+      let codeArray = codeLetter.split('');
+      k = codeArray.length;
+      let count = 0;
+
+      while(k){
+        stack.push(codeArray[k-2] + codeArray[k-1]);
+        k = k - 2;
+        count ++;
+      }
+
+      if(stack.empty){
+        continue;
+      }
+      let morseCode = "";
+      for(let j = 0; j < 5; j++){
+        let code = stack.pop();
+        if(code.indexOf('1') < 0) {
+          continue;
+        }
+        if(code === "10"){
+          morseCode += ".";
+        }
+        if(code === "11"){
+          morseCode += "-";
+        }
+      }
+      result += MORSE_TABLE[morseCode];
+    }
+    return result;
+}
 module.exports = {
     decode
 }
